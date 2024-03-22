4ad5cb5ec82f8d43af74755caddf1b1188c71b0e
Noah Mekonnen <noahmekonnen0@gmail.com>
2024-03-22 06:26:54 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useRef, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { v4 as uuid } from 'uuid';
 
 function App() {
+  const DECK_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+
+  const id = useRef()
+
+  const [cards, setCards] = useState([])
+
+  useEffect(() => async () => {
+    const res = await axios.get(DECK_URL)
+    id.current = res.data.deck_id
+  }, [])
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const DRAW_URL = `https://deckofcardsapi.com/api/deck/${id.current}/draw/?count=1`
+    if (cards.length != 52) {
+      const res = await axios.get(DRAW_URL)
+      setCards(cards => [...cards, { imgSrc: res.data.cards[0].image }])
+    } else {
+      alert("Error: no cards remaining!")
+    }
+  }
+
+  const shuffle = async (deck_id) => {
+    const SHUFFLE_URL = `https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`
+    const res = await axios.get(SHUFFLE_URL)
+    setCards([])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => shuffle(id.current)}>Shuffle Deck</button>
+      <button onClick={handleClick}>Gimme A Card</button>
+      {cards.map(({ imgSrc }) => <img key={uuid()} src={imgSrc} />)}
     </div>
   );
 }
